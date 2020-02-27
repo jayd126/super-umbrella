8f7518e359b3c799c546a2e92744fd5e70796f52
Jayden Duck <c3355723@uon.edu.au>
2020-02-27 17:24:58 +1100

    commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() //needs to start with int to return type %d formats an integer
+{
+    float x;
+    float y;
+    printf("Enter a number: ");
+    scanf("%f", &x);
+    y = x/1-x;
+    printf("y: %f\n", y);
+    return 0;
+}
